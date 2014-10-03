73b4d512c2452cea1dd454cfe75e65c6f48f84b1
Miclle <miclle.zheng@gmail.com>
2014-10-03 18:44:03 +0800

    Add docs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  resources :docs, :path => :file, :only => [:show, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
